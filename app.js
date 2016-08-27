8cbddaa2085b266dee5866fb64ee1dec5f5dc8e6
Jasper Nalbach <derjasper@posteo.de>
2016-08-27 15:45:28 +0200

    send mails on error + reschedule on error + better error handling in fuel-spain

--- app.js
+++ app.js
@@ -1,89 +1,116 @@
-var schedule = require('node-schedule');
+var schedule = require('node-schedule')
+var nodemailer = require('nodemailer')
+var sendmailTransport = require('nodemailer-sendmail-transport')
 
 var plugins = {
-  "fuelSpain": require('./fetcher/fuel-spain.js')
-};
+  'fuelSpain': require('./fetcher/fuel-spain.js')
+}
 
 var config = {
-  "mail": "",
-  "fetcher": {
-    "fuelSpain": {
+  'mail': '',
+  'fetcher': {
+    'fuelSpain': {
       settings: {
-        targetFile: "../simple-geojson-server/data/fuelSpain.geojson",
-        serverSocket: "/tmp/simple-geojson-server.sock",
-        serverService: "fuelSpain"
+        targetFile: '../simple-geojson-server/data/fuelSpain.geojson',
+        serverSocket: '/tmp/simple-geojson-server.sock',
+        serverService: 'fuelSpain'
       },
       schedule: {
         hour: 5
-      }
+      },
+      rescheduleOnFailAfterMinutes: 15
     }
   }
-};
+}
 
 // TODO settings manager
 // TODO plugin manager
 // TODO move deploy-step out of fetcher
 
 // process args
-var runOnce = false;
-if (process.argv.length >= 3 && process.argv[2].trim() == "run-once") {
-  runOnce = true;
+var runOnce = false
+if (process.argv.length >= 3 && process.argv[2].trim() === 'run-once') {
+  runOnce = true
 }
 
-console.log("Starting fetcher");
+console.log('Starting fetcher')
 
 for (var key in config.fetcher) {
   if (!runOnce) {
-    schedule.scheduleJob(config.fetcher[key].schedule, function(){
-      run(key);
-    });
-  }
-  else {
-    run(key);
+    schedule.scheduleJob(config.fetcher[key].schedule, function () {
+      run(key)
+    })
+  } else {
+    run(key)
   }
 }
 
-function run(plugin) {
-  console.log("["+plugin+"]", "Run");
+function run (plugin) {
+  console.log('[' + plugin + ']', 'Run')
   // settings
-  var settings = config.fetcher[plugin].settings;
+  var settings = config.fetcher[plugin].settings
   // fetch data
-  console.log("["+plugin+"]", "Fetch...");
-  plugins[plugin].fetch(settings,function(err,data) {
+  console.log('[' + plugin + ']', 'Fetch...')
+  plugins[plugin].fetch(settings, function (err, data) {
     if (err) {
-      handleError(plugin, "Fetch failed", err);
-    }
-    else {
-      console.log("["+plugin+"]", "Process...");
-      plugins[plugin].process(settings,data, function(err,data) {
+      handleError(plugin, 'Fetch failed', err)
+    } else {
+      console.log('[' + plugin + ']', 'Process...')
+      plugins[plugin].process(settings, data, function (err, data) {
         if (err) {
-          handleError(plugin, "Process failed", err);
-        }
-        else {
-          console.log("["+plugin+"]", "Test...");
-          plugins[plugin].test(settings,data, function(err, data) {
+          handleError(plugin, 'Process failed', err)
+        } else {
+          console.log('[' + plugin + ']', 'Test...')
+          plugins[plugin].test(settings, data, function (err, data) {
             if (err) {
-              handleError(plugin, "Test failed", err);
-            }
-            else {
-              console.log("["+plugin+"]", "Deploy...");
-              plugins[plugin].deploy(settings,data, function(err) {
+              handleError(plugin, 'Test failed', err)
+            } else {
+              console.log('[' + plugin + ']', 'Deploy...')
+              plugins[plugin].deploy(settings, data, function (err) {
                 if (err) {
-                  handleError(plugin, "Deploy failed", err);
+                  handleError(plugin, 'Deploy failed', err)
+                } else {
+                  console.log('[' + plugin + ']', 'Finished')
                 }
-                else {
-                  console.log("["+plugin+"]", "Finished");
-                }
-              });
+              })
             }
-          });
+          })
         }
-      });
+      })
     }
-  });
+  })
 }
 
-function handleError(plugin, text, err) {
-  console.error("["+plugin+"]", "Error: " + text + ": ", err);
-  // TODO send mail
+// set up mail
+var transporter = nodemailer.createTransport(sendmailTransport({}))
+
+// error handler
+function handleError (plugin, text, err) {
+  console.error('[' + plugin + ']', 'Error: ' + text + ': ', err)
+
+  // retry later
+  if (config.rescheduleOnFailAfterMinutes > 0 && !runOnce) {
+    var date = new Date(Date.now() + config.rescheduleOnFailAfterMinutes * 60000)
+    schedule.scheduleJob(date, function () {
+      run(plugin)
+    })
+  }
+
+  // send mail
+  if (config.mail !== '') {
+    var mailData = {
+      from: config.mail,
+      to: config.mail,
+      subject: 'data-fetcher: ' + plugin + ' failed',
+      text: text + ': ' + err
+    }
+
+    transporter.sendMail(mailData, function (err) {
+      if (err) {
+        console.log('Sending mail failed', err)
+      } else {
+        console.log('Mail sent')
+      }
+    })
+  }
 }
--- fetcher/fuel-spain.js
+++ fetcher/fuel-spain.js
@@ -1,142 +1,141 @@
-var net = require('net');
-var request = require('request');
-var fs = require('fs');
+var net = require('net')
+var request = require('request')
+var fs = require('fs')
 
 // Fetches data from http://www.geoportalgasolineras.es, stores it as .geojson file and deploys it to simple-geojson-server
 
-function fetchData(settings, callback) {
-      request.get(
-        {
-          url: 'https://sedeaplicaciones.minetur.gob.es/ServiciosRESTCarburantes/PreciosCarburantes/EstacionesTerrestres/',
-          headers: {
-            'Accept': 'application/json'
-          }
-        },
-        function(error, response, body) {
-          if (!error && response.statusCode == 200) {
-            var data = body;
-            callback(null, data);
-          }
-          else if (!error) {
-            callback(new Error("Status Code: " + response.statusCode), null);
-          }
-          else {
-            callback(error, null);
-          }
-        });
-  }
+function fetchData (settings, callback) {
+  request.get(
+    {
+      url: 'https://sedeaplicaciones.minetur.gob.es/ServiciosRESTCarburantes/PreciosCarburantes/EstacionesTerrestres/',
+      headers: {
+        'Accept': 'application/json'
+      }
+    },
+    function (error, response, body) {
+      if (error) {
+        callback(error, null)
+      } else if (response.statusCode !== 200) {
+        callback(new Error('Status Code: ' + response.statusCode), null)
+      } else if (body.indexOf('SERVICIO NO DISPONIBLE') !== -1) {
+        callback(new Error('Service down'), null)
+      } else {
+        callback(null, body)
+      }
+    })
+}
 
-  function processData(settings, rawData, callback) {
-    process.nextTick(function(){
+function processData (settings, rawData, callback) {
+  process.nextTick(function () {
+    try {
+      var list = JSON.parse(rawData).ListaEESSPrecio
+
+      var features = []
+
+      for (var i = 0; i < list.length; i++) {
+        /*
+        *GPR : Gasolina 95 (G.Protección) // "Precio Gasolina 95 Protección"
+        *G98 : Gasolina 98 // "Precio Gasolina  98"
+        *GOA : Gasóleo A habitual // "Precio Gasoleo A"
+        *NGO : Nuevo gasóleo A // "Precio Nuevo Gasoleo A"
+        GOB : Gasóleo B
+        GOC : Gasóleo C
+        *BIO : Biodiésel // "Precio Biodiesel"
+        G95 : Gasolina 95
+        *BIE : Bioetanol // "Precio Bioetanol"
+        GLP : Gases licuados del petróleo
+        *GNC : Gas natural comprimido // "Precio Gas Natural Comprimido"
+        */
+
+        // check if entry is suitable
+        if (list[i]['Latitud'] == null || list[i]['Longitud (WGS84)'] == null) {
+          continue
+        }
+
+        var lat = parseFloat(list[i]['Latitud'].replace(',', '.'))
+        var lng = parseFloat(list[i]['Longitud (WGS84)'].replace(',', '.'))
+        var properties = {
+          brand: list[i]['Rótulo'],
+          address: list[i]['Dirección'],
+          place: list[i]['Municipio'],
+          state: list[i]['Provincia'],
+          openingTimes: list[i]['Horario'],
+          price_gpr: list[i]['Precio Gasolina 95 Protección'],
+          price_g98: list[i]['Precio Gasolina  98'],
+          price_goa: list[i]['Precio Gasoleo A'],
+          price_ngo: list[i]['Precio Nuevo Gasoleo A'],
+          price_bio: list[i]['Precio Biodiesel'],
+          price_bie: list[i]['Precio Bioetanol'],
+          price_gnc: list[i]['Precio Gas Natural Comprimido']
+        }
+
+        features.push({
+          'type': 'Feature',
+          'id': i + '',
+          'geometry': {
+            'type': 'Point',
+            'coordinates': [lng, lat]
+          },
+          'properties': properties
+        })
+      }
+
+      callback(null, {
+        'type': 'FeatureCollection',
+        'features': features
+      })
+    } catch (err) {
+      callback(err, null)
+    }
+  })
+}
+
+function testData (settings, data, callback) {
+  process.nextTick(function () {
+    if (data.features.length === 0) {
+      callback(new Error('Empty result!'), data)
+    } else {
+      callback(null, data)
+    }
+  })
+}
+
+function deployData (settings, data, callback) {
+  fs.writeFile(settings.targetFile, JSON.stringify(data), function (err) {
+    if (err) {
+      callback(err)
+    } else {
       try {
-        var list = JSON.parse(rawData).ListaEESSPrecio;
-        var features = [];
-
-        for (var i = 0; i < list.length; i++) {
-          /*
-          *GPR : Gasolina 95 (G.Protección) // "Precio Gasolina 95 Protección"
-          *G98 : Gasolina 98 // "Precio Gasolina  98"
-          *GOA : Gasóleo A habitual // "Precio Gasoleo A"
-          *NGO : Nuevo gasóleo A // "Precio Nuevo Gasoleo A"
-          GOB : Gasóleo B
-          GOC : Gasóleo C
-          *BIO : Biodiésel // "Precio Biodiesel"
-          G95 : Gasolina 95
-          *BIE : Bioetanol // "Precio Bioetanol"
-          GLP : Gases licuados del petróleo
-          *GNC : Gas natural comprimido // "Precio Gas Natural Comprimido"
-          */
-
-          // check if entry is suitable
-          if (list[i]["Latitud"] == null || list[i]["Longitud (WGS84)"] == null) {
-            continue;
+        var success = false
+        var client = net.connect(settings.serverSocket, function () {
+          client.write('updateService ' + settings.serverService)
+        })
+        client.on('data', function (data) {
+          if (data.toString().trim() === 'updating service') {
+            success = true
           }
-
-          var lat = parseFloat(list[i]["Latitud"].replace(",", "."));
-          var lng = parseFloat(list[i]["Longitud (WGS84)"].replace(",", "."));
-          var properties = {
-            brand: list[i]["Rótulo"],
-            address: list[i]["Dirección"],
-            place: list[i]["Municipio"],
-            state: list[i]["Provincia"],
-            openingTimes: list[i]["Horario"],
-            price_gpr: list[i]["Precio Gasolina 95 Protección"],
-            price_g98: list[i]["Precio Gasolina  98"],
-            price_goa: list[i]["Precio Gasoleo A"],
-            price_ngo: list[i]["Precio Nuevo Gasoleo A"],
-            price_bio: list[i]["Precio Biodiesel"],
-            price_bie: list[i]["Precio Bioetanol"],
-            price_gnc: list[i]["Precio Gas Natural Comprimido"],
-          };
-
-          features.push({
-            "type": "Feature",
-            "id": i+"",
-            "geometry": {
-              "type": "Point",
-              "coordinates": [lng, lat]
-            },
-            "properties": properties
-          });
-        }
-
-        callback(null, {
-          "type": "FeatureCollection",
-          "features": features
-        });
+          client.end()
+        })
+        client.on('error', function (err) {
+          callback(err)
+        })
+        client.on('end', function () {
+          if (!success) {
+            callback(new Error('Could not force update on simple-geojson-server'))
+          } else {
+            callback(null)
+          }
+        })
+      } catch (er) {
+        callback(er)
       }
-      catch (err) {
-        callback(err, null);
-      }
-    });
-  }
+    }
+  })
+}
 
-  function testData(settings, data, callback) {
-    // TODO test
-    process.nextTick(function(){
-      callback(null, data);
-    });
-  }
-
-  function deployData(settings, data, callback) {
-    fs.writeFile(settings.targetFile, JSON.stringify(data), function (err) {
-      if (err) {
-        callback(err);
-      }
-      else {
-        try {
-          var success = false;
-          var client = net.connect(settings.serverSocket, function () {
-            client.write("updateService "+settings.serverService);
-          });
-          client.on('data', function (data) {
-            if (data.toString().trim() == "updating service") {
-              success = true;
-            }
-            client.end();
-          });
-          client.on('error', function(err) {
-            callback(err);
-          });
-          client.on('end', function () {
-            if (!success) {
-              callback(new Error("Could not force update on simple-geojson-server"));
-            }
-            else {
-              callback(null);
-            }
-          });
-        }
-        catch (er) {
-          callback(er);
-        }
-      }
-    });
-  }
-
-  module.exports = {
-    fetch: fetchData,
-    process: processData,
-    test: testData,
-    deploy: deployData
-  };
+module.exports = {
+  fetch: fetchData,
+  process: processData,
+  test: testData,
+  deploy: deployData
+}
